62677db49a7d51ccf109a139bcd45454403649be
tpacun <62911461+tpacun@users.noreply.github.com>
2022-03-17 19:32:43 -0400

    adding tests

--- codewars/8th_kyu/javascript/hello_world.js
+++ codewars/8th_kyu/javascript/hello_world.js
@@ -3,6 +3,19 @@
 // Description:
 // Make a simple function called greet that returns the most-famous "hello world!".
 
+// Tests:
+
+// const assert = require("chai").assert;
+
+// describe("Testing function", function() {
+//   it("Is it a function?", function() {
+//     assert.strictEqual(typeof greet, "function", "greet should be a function");
+//   });
+//   it("Correct return-value?", function() {
+//     assert.strictEqual(greet(), "hello world!");
+//   });
+// });
+
 // Solution:
 
 // Write a function "greet" that returns "hello world!"
