cd816d04773467ba38420f15abdea168d9fefb6c
tpacun <62911461+tpacun@users.noreply.github.com>
2022-03-17 19:31:15 -0400

    daily codewars challenge

---
+++ codewars/8th_kyu/javascript/hello_world.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Solution:
+
+// Write a function "greet" that returns "hello world!"
+
+function greet() {
+    return 'hello world!'
+  }
