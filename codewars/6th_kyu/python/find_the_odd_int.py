36db8b75d2a13b5c6d65b7895a675d90458e6037
tpacun <62911461+tpacun@users.noreply.github.com>
2022-03-24 21:53:51 -0400

    adding clean refactor solution to find_the_odd_int.py

--- codewars/6th_kyu/python/find_the_odd_int.py
+++ codewars/6th_kyu/python/find_the_odd_int.py
@@ -11,7 +11,7 @@
 # [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 # [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-# Solution:
+# Solution - ugly:
 
 def find_it(seq):
     dictionary_keys = list(set(seq))
@@ -23,3 +23,10 @@ def find_it(seq):
     for num in dictionary_count:
         if dictionary_count[num] % 2 == 1:
             return num
+
+# Refactor v2 - much cleaner solution
+
+def find_it(seq):
+    for x in seq:
+        if seq.count(x) % 2 == 1:
+            return x
