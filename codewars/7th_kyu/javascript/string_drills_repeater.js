71e1dadddd846788141ff99a15b620faad57f93a
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-06 11:54:32 -0400

    add string_drills_repeater.js, returns string repeated a variable number of times

---
+++ codewars/7th_kyu/javascript/string_drills_repeater.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/javascript
+
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example: (Input1, Input2 --> Output)
+// "a", 5 --> "aaaaa"
+
+// Solution: 
+
+function repeater(string, n){
+  let newString = '';
+  let range = [...Array(n)].forEach(() => newString += string)
+  return newString
+}
