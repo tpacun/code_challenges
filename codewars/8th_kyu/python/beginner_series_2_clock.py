4f669698c338798298595e4372078a2bb2a3a368
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-13 13:21:46 -0400

    add beginner_series_2_clock.py

---
+++ codewars/8th_kyu/python/beginner_series_2_clock.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/python
+
+# Clock shows h hours, m minutes and s seconds after midnight.
+
+# Your task is to write a function which returns the time since midnight in milliseconds.
+
+# Example:
+# h = 0
+# m = 1
+# s = 1
+
+# result = 61000
+# Input constraints:
+
+# 0 <= h <= 23
+# 0 <= m <= 59
+# 0 <= s <= 59
+
+# Solution:
+
+def past(h, m, s):
+    return 1000 * s + m * 60000 + h * 3600000
