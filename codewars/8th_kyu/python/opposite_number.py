75370a9d54a6402862c1514e2ad97f17668ae144
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-13 09:40:24 -0400

    add opposite_number.py

---
+++ codewars/8th_kyu/python/opposite_number.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+# Very simple, given an integer or a floating-point number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+# Solution:
+
+def opposite(number):
+    return number * -1
