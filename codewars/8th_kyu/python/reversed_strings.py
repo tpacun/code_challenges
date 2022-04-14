73152977997d9ee1bfc20f61d043e51859ac33b9
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-14 11:24:43 -0400

    added reversed_strings.py

---
+++ codewars/8th_kyu/python/reversed_strings.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# Solution:
+
+def solution(string):
+    return string[::-1]
