09b9746547f5f1513e4d10797df57ba8bf94d9ba
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-17 14:00:54 -0400

    add even_or_odd.js

---
+++ codewars/8th_kyu/javascript/even_or_odd.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// Solution:
+
+function even_or_odd(number) {
+    return number % 2 ? 'Odd':'Even'
+  }
