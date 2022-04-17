cda29552cadc8a401339a4713c7c9b78707154ae
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-17 13:58:16 -0400

    add even_or_odd.py

---
+++ codewars/8th_kyu/python/even_or_odd.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# Solution:
+
+def even_or_odd(number):
+    return 'Odd' if number % 2 else 'Even'
