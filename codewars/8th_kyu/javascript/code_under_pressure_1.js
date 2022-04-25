1def737d848a790797cfe97f68cedfc02df715d5
tpacun <62911461+tpacun@users.noreply.github.com>
2022-04-25 09:58:44 -0400

    adding code_under_pressure_1.js

---
+++ codewars/8th_kyu/javascript/code_under_pressure_1.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// You need to double the integer and return it.
+
+// Solution:
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
