57179665b37c241661b0fd8d69c7e7ef662f36c7
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-04-27 09:58:15 -0400

    adding returning_strings.py

---
+++ codewars/8th_kyu/python/returning_strings.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+# Solution:
+
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
