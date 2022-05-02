adbd2137b1f250d41f9c0c470472fcea5a501a34
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-02 16:47:24 -0400

    add parse_nice_int_from_char.js

--- codewars/8th_kyu/javascript/volume_of_a_cuboid.js
+++ codewars/8th_kyu/javascript/volume_of_a_cuboid.js
@@ -1,7 +1,5 @@
 // https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
 
-// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
-
 // Solution:
 
 class Kata {
