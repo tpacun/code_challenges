929cc8bc10238c9379d72523ae9347e9d8ee3572
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-04 22:17:35 -0400

    adding messi_goals_function.js

---
+++ codewars/8th_kyu/javascript/messi_goals_function.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+// Solution:
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
