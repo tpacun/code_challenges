6a3c59ab733034ed989f7c2ab8091d7e81c89a42
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-04 22:28:51 -0400

    addig reverse_words.js

---
+++ codewars/7th_kyu/javascript/reverse_words.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Solution:
+
+function reverseWords(str) {
+    return str.split(' ').map((word) => word.split('').reverse().join('')).join(' ')
+  }
