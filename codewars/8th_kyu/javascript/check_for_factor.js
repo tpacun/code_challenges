e78433eabff3283f94f14a68e5e73b43c655c09c
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-10 09:49:09 -0400

    adding check_for_factor.js

---
+++ codewars/8th_kyu/javascript/check_for_factor.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// Solution:
+
+function checkForFactor (base, factor) {
+    return !(base % factor)
+  }
