c81de96efd8e3a868459be005a111604d42fe456
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-26 08:56:22 -0400

    adding reverse_list_order.js

---
+++ codewars/8th_kyu/javascript/reverse_list_order.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/javascript
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Solution:
+function reverseList(list) {
+    return list.reverse()
+  }
