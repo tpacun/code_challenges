dd0805761e9e5103e95634349d94206ae678d8e9
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-27 22:16:56 -0400

    adding squaring_an_argument.js

---
+++ codewars/8th_kyu/javascript/squaring_an_argument.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Solution:
+const square = (num) => {return num ** 2}
