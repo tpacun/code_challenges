9f0b4a565ca5b6c0dcb23c70005999de4d68f372
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-30 21:59:00 -0400

    adding get_char_from_ascii_value.js

---
+++ codewars/8th_kyu/javascript/get_char_from_ascii_value.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Solution:
+
+function getChar(c){
+    return String.fromCharCode(c)
+  }
