5c0950ef352fbd8aea2870eb4f4c9cd60f9c60d9
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-06-05 12:22:55 -0400

    adding terminal_game_combat_function.js

---
+++ codewars/8th_kyu/javascript/terminal_game_combat_function.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/586c1cf4b98de0399300001d/train/javascript
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+// Solution:
+
+function combat(health, damage) {
+    return health - damage < 0 ? 0:health - damage
+  }
