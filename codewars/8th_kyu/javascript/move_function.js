6ef05fce776dd203516c07a02fc02819b6c1cb5c
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-06-07 09:22:31 -0400

    adding move_function.js

---
+++ codewars/8th_kyu/javascript/move_function.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Solution
+
+function move (position, roll) {
+    return position + roll * 2
+  }
