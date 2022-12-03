52899c87df03a57d47101991b7c9fd2e63ccb20d
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-12-03 18:17:26 -0500

    Adding AoC Day 3, Part 2

--- adventofcode/javascript/day3/main.js
+++ adventofcode/javascript/day3/main.js
@@ -54,6 +54,12 @@ rucksackUniques.forEach((set, i) => {
 // Gets utf-16 char codes for each: a is 97, A is 65 (to calculate prios)
 console.log('Character Codes:', 'a'.charCodeAt(0), 'A'.charCodeAt(0))
 
+// Revision: writing function to be reused in part two
+
+function getNumericPrio(str) {
+    return str === str.toUpperCase() ? str.charCodeAt(0) - 38:str.charCodeAt(0) - 96
+}
+
 const rucksackNumericPrios = rucksackMatches.map((arr) => {
     if (arr[0] === arr[0].toUpperCase()) {
         return [arr[0].charCodeAt(0) - 38]
@@ -69,5 +75,39 @@ rucksackNumericPrios.forEach(arr => {
 
 console.log(`Part One Answer: ${sum}`)
 
+// Part Two
 
+// The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
 
+// Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+// vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// And the second group's rucksacks are the next three lines:
+
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw
+// In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+// Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+// Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+
+let badges = [];
+rucksackUniques.forEach((set, i) => {
+    if (!(i % 3)) {
+        set.forEach(char => {
+            if (rucksackUniques[i + 1].has(char) && rucksackUniques[i + 2].has(char)) {
+                badges.push(char)
+            }
+        })
+    }
+})
+
+let partTwoSum = 0;
+badges.forEach((str) => {
+    partTwoSum += getNumericPrio(str)
+})
+console.log(`Part Two Answer: ${partTwoSum}`)
