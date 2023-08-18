5b184c40d117d4081fbae7243e7063b2e633b8c1
tpacun <62911461+tpacun@users.noreply.github.com>
2023-08-18 09:59:01 -0400

    updating

--- adventofcode/javascript/day7/index.js
+++ adventofcode/javascript/day7/index.js
@@ -1,2 +1,33 @@
-const daySevenFunctions = require('./main')
-console.log(daySevenFunctions.parseText('./input.txt'))
+const fs = require('fs')
+
+const inputText = fs.readFileSync('./testInput.txt').toString()
+
+const filepath = [];
+const files = {};
+
+function traverse(obj, arr) {
+    return arr.reduce((acc, curr) => acc ? acc[curr] : undefined, obj);
+  }
+
+const inputArray = inputText.split('$')
+const parsedArray = inputArray.filter((c) => {return c !== ''} )
+const objInstructions = parsedArray.map((c) => {
+    let removeWhitespace = c.trim()
+    let arrayCR = removeWhitespace.split('\r\n')
+    return {command: arrayCR[0].split(' '),
+        responses: arrayCR.length > 1 ? arrayCR.slice(1):null
+    }
+})
+
+objInstructions.forEach((instruction) => {
+    if (instruction.command[0] === 'cd') {
+        if (instruction.command[1] === '..') {
+            filepath.pop()
+        } else {
+            filepath.push(instruction.command[1])
+            let locToEdit = traverse(files, filepath)
+            console.log(locToEdit)
+            
+        }
+    }
+})
