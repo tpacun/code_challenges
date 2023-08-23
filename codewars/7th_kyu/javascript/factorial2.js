f33a78e88b0b841114c2b70fbb6f86ad0e3b29ba
tpacun <62911461+tpacun@users.noreply.github.com>
2023-08-23 11:26:40 -0400

    refactor factorial2.js

--- codewars/7th_kyu/javascript/factorial2.js
+++ codewars/7th_kyu/javascript/factorial2.js
@@ -17,3 +17,12 @@ function factorial(n){
     }
     return total
   }
+
+//   Refactor:
+function factorial(n){
+    let total = 1
+    for (i=n; i > 0; i--) {
+      total *= i
+    }
+    return total
+  }
