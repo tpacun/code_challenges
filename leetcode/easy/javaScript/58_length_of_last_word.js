b1210ebd43642da4f8006f38468d6d147455d4d3
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2024-03-16 17:07:12 -0400

    added solution on 58_length_of_last_words

--- leetcode/easy/javaScript/58_length_of_last_word.js
+++ leetcode/easy/javaScript/58_length_of_last_word.js
@@ -12,3 +12,23 @@ var lengthOfLastWord = function (s) {
   const sFiltered = sSplit.filter((c) => c.length !== 0);
   return sFiltered[sFiltered.length - 1].length;
 };
+
+// Solution 2
+
+// without using built-in functions
+
+var lengthOfLastWord = function (s) {
+  let returnString = "";
+  for (i = s.length - 1; i >= 0; i--) {
+    if (s[i] == " " && returnString.length > 0) {
+      return returnString.length;
+    } else if (s[i] == " " && returnString.length == 0) {
+      continue;
+    } else {
+      returnString = s[i].concat(returnString);
+      if (i == 0) {
+        return returnString.length;
+      }
+    }
+  }
+};
